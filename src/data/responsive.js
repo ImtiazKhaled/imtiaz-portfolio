e63326c2dd7807693f9008dd085e1c4e316b7b17
ImtiazKhaled <44986448+ImtiazKhaled@users.noreply.github.com>
2019-07-13 00:04:58 +0600

    Updates

--- src/data/responsive.js
+++
@@ -1,9 +0,0 @@
-import { stylesXS } from '../styles/xSmallStyles';
-import { stylesS } from '../styles/smallStyles';
-import { stylesM } from '../styles/mediumStyles';
-import { stylesL } from '../styles/largeStyles';
-import { stylesXL } from '../styles/xLargeStyles';
-
-export const responsive = [
-    stylesXS, stylesS, stylesM, stylesL, stylesXL
-]
